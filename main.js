7c2a13a1628c9b30b8aa8975620ef42d1b239e79
Cleiton Lahm <cleijllahm@gmail.com>
2023-04-26 15:32:03 -0300

    add main.js

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const form = document.querySelector("#form-atividade");
+const imgAprovado = "<img src='./imagens/aprovado.png' />"
+const imgReprovado = "<img src='./imagens/reprovado.png' />"
+
+let linhas = "";
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const inputNomeAtividade = document.querySelector("#nome-atividade");
   const inputNotaAtividade = document.querySelector("#nota-atividade");
 
-  alert(`atividade: ${inputNomeAtividade.value} - nota: ${inputNotaAtividade.value}`)
+  let linha = "<tr>";
+  linha += `<td>${inputNomeAtividade.value}</td>`;
+  linha += `<td>${inputNotaAtividade.value}</td>`;
+  linha += `<td>${inputNotaAtividade.value >= 7 ? 'Aprovado' : 'Reprovado'}</td>`;
+  linha += '</tr>';
+
+  linhas += linha;
+
+  const corpoTabela = document.querySelector("tbody");
+  corpoTabela.innerHTML = linhas;
+
+  inputNomeAtividade.value = "";
+  inputNotaAtividade.value = "";
+  
 })
